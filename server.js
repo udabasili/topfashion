00cfe7eb18003015b939bacc44eb4277963e043c
udabasili <udbasili@yahoo.com>
2020-03-01 07:55:40 -0400

    added serve and more client functionality

--- server.js
+++ server.js
@@ -17,9 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors());
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname,'/client/build')))
-}
+app.use(express.static(path.join(__dirname,'/client/build')))
+
 
 //routers
 app.use("/auth", userRouter)
